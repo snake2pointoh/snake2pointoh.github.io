e2823c7f3e3e5d657e2e87bbcf3f694322cc9f2f
snake2pointoh <45461838+snake2pointoh@users.noreply.github.com>
2019-09-10 13:16:11 -0600

    added my own solo project

---
+++ personalproject/sketch.js
@@ -0,0 +1,21 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+let backgroundColour = 255;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(backgroundColour);
+  console.log(width + " Width " + eight + " Height ");
+}
+
+
+function draw() {
+  line(mouseX, mouseY, pmouseX, pmouseY)
+}
+
