75518ba8745858f6c0c7aa278c2ae7a2f05798db
snake2pointoh <45461838+snake2pointoh@users.noreply.github.com>
2019-09-11 14:11:46 -0600

    added first project

---
+++ Interactivescene/sketch.js
@@ -0,0 +1,28 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+//https://en.wikipedia.org/wiki/3D_projection//
+//https://stackoverflow.com/questions/6139451/how-can-i-convert-3d-space-coordinates-to-2d-space-coordinates//
+
+function Vector3(x,y,z){
+  this.x = x
+  this.y = y
+  this.z = z
+}
+
+let backgroundColour = 220;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(backgroundColour);
+  console.log("Width " + width +" Height " + height)
+}
+
+function draw() {
+
+}
+
