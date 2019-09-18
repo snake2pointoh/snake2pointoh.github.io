6201da76230942dea72a60512c12eb76f84d4114
snake2pointoh <45461838+snake2pointoh@users.noreply.github.com>
2019-09-18 14:12:22 -0600

    Update sketch.js

--- Interactivescene/sketch.js
+++ Interactivescene/sketch.js
@@ -8,7 +8,6 @@
 //https://en.wikipedia.org/wiki/3D_projection//
 //https://stackoverflow.com/questions/6139451/how-can-i-convert-3d-space-coordinates-to-2d-space-coordinates//
 
-let sceneNO = 1;
 let backgroundColour = 255;
 let angle = 0;
 
@@ -16,35 +15,90 @@ let angle = 0;
 
 let xAngle = 0;
 let yAngle = 0;
+let zAngle = 0;
+let xSize = 100;
+let ySize = 100;
+let zSize = 100;
+
+let scaleBy = 5;
+
+let rotating = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   background(backgroundColour);
   console.log("Width " + width +" Height " + height)
-}
-
-function draw() {
-  if(sceneNO === 1){
-    menu();
-  }
   angleMode(DEGREES)
 }
 
-function menu(){
+function draw() {
   background(backgroundColour);
-  xAngle = ((mouseX - width/2)/10);
-  yAngle = ((mouseY - height/2)/10)*-1;
-  rotateWithMouse()
-  box(100, 100, 300)
+  XYZ()
+  draw3D()
+  Ui();
+}
+
+function Ui(){
+  push()
+  translate(width/2*-1, height/2*-1)
+  rect(20, 20, 80, 80,)
+  if(rotating === true){
+    //text(Rot, 25, 25)
+  }
+  else{
+    //test(Size, 25, 25)
+  }
+  pop()
+}
+
+function draw3D(){
+  push()
+  rotateX(xAngle)
+  rotateY(yAngle)
+  rotateZ(zAngle)
+  box(xSize, ySize, zSize)
+  pop()
+}
+
+function XYZ(){
+  if(rotating === true){
+    if(keyIsDown(88)){
+      xAngle += 5
+    }
+    if(keyIsDown(89)){
+      yAngle += 5
+    }
+    if(keyIsDown(90)){
+      zAngle += 5
+    }
+  }
+  else{
+    if(keyIsDown(88)){
+      xAngle += 5
+    }
+    if(keyIsDown(89)){
+      yAngle += 5
+    }
+    if(keyIsDown(90)){
+      zAngle += 5
+    }
+  }
+}
+
+function keyPressed(){
+  if(key === "r"){
+    rotating = true;
+  }
+  else if(key === "s"){
+    rotating = false;
+  }
+  if(key === UP_ARROW){
+    scaleBy *= -1
+  }
 }
 
 function windowResized(){
   setup()
-}
-
-function rotateWithMouse(){
-  rotateY(xAngle)
-  rotateX(yAngle)
 }
 
 //LERPPPP//v0 to v1 by t from 0 to 1//
