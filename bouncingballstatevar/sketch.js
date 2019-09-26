bbb97e4567e51efb51b5388a4385c72c86610bf6
snake2pointoh <45461838+snake2pointoh@users.noreply.github.com>
2019-09-26 12:23:21 -0600

    started major project

--- bouncingballstatevar/sketch.js
+++ bouncingballstatevar/sketch.js
@@ -15,29 +15,87 @@ let Ypos;
 let dx = 5
 let dy = 5
 
-let mode = 'circle';
+let ballMode = 'circle';
+let sceneMode = 'menu'
+
+class button{
+  constructor(x1, y1, w1, h1, name1){
+    
+    //x and y position of the button//
+    this.x = x1
+    this.y = y1
+    
+    //width and height of the button//
+    this.w = w1
+    this.h = h1
+    
+    //button color//
+    this.name = name1
+
+    this.x -= (this.w/2)
+    this.y -= (this.h/2)
+  }
+
+  //draws the button//
+  show(){
+
+    fill(255)
+    rect(this.x, this.y, this.w, this.h)
+    fill(0)
+    textSize(20)
+    text(this.name, this.x, this.y)
+  }
+
+  //checks if the mouse is over the button//
+  pressed(){
+    if((mouseX >= this.x && mouseX <= this.x + this.w) && (mouseY >= this.y && mouseY <= this.y + this.h)){
+      return true
+    }
+    else {
+      return false
+    }
+  }
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   console.log(width + " Width " + height + " Height ");
   Xpos = width/2;
   Ypos = height/2;
-  
+  rectButton = new button(width/2, height/2 - 80, 200, 100,"Rect")
+  ballButton = new button(width/2, height/2 + 80, 200, 100,"ball")
+  backButton = new button(100, 100, 100, 100, "Back")
 }
 
 
 function draw() {
-  fill(0)
   background(backgroundColour);
+  if(sceneMode === 'menu'){
+    startScreen()
+  }
+  if(sceneMode === 'ballscreen'){
+    ballScreen()
+  }
+}
+
+function startScreen(){
+  rectButton.show()
+  ballButton.show()
+}
+
+function ballScreen(){
+  fill(0)
+  
   //left right movement//
-  if(mode === 'circle'){
+  if(ballMode === 'circle'){
     circleBounce()
   }
-  else if (mode === 'rect'){
+  else if (ballMode === 'rect'){
     rectBounce()
   }
   Xpos += dx
   Ypos += dy
+  backButton.show()
 }
 
 function windowResized(){
@@ -64,15 +122,25 @@ function rectBounce(){
   }
 }
 
-function keyPressed(){
-  if(key === 'r'){
-    mode = 'rect'
-    Xpos = width/2;
-    Ypos = height/2;
+function mouseClicked(){
+  if(sceneMode === 'menu'){
+    if(rectButton.pressed()){
+      ballMode = 'rect'
+      Xpos = width/2;
+      Ypos = height/2;
+      sceneMode = 'ballscreen'
+
+    }
+    if(ballButton.pressed()){
+      ballMode = 'circle'
+      Xpos = width/2;
+      Ypos = height/2;
+      sceneMode = 'ballscreen'
+    }
   }
-  if(key === 'c'){
-    mode = 'circle'
-    Xpos = width/2;
-    Ypos = height/2;
+  if(sceneMode === 'ballscreen'){
+    if(backButton.pressed()){
+      sceneMode = 'menu'
+    }
   }
 }
--- majorproject/sketch.js
+++ majorproject/sketch.js
@@ -8,14 +8,48 @@
 
 let backgroundColour = 255;
 
+class button{
+  constructor(x1, y1, w1, h1, name1){
+    
+    //x and y position of the button//
+    this.x = x1
+    this.y = y1
+    
+    //width and height of the button//
+    this.w = w1
+    this.h = h1
+    
+    //button color//
+    this.name = name1
+
+    //sets pos to center
+    this.x -= (this.w/2)
+    this.y -= (this.h/2)
+  }
+
+  //draws the button//
+  show(){
+    rectMode(CORNER)
+    textAlign(CENTER, CENTER)
+
+    fill(255)
+    rect(this.x, this.y, this.w, this.h)
+    
+    fill(0)
+    textSize(20)
+    text(this.name, this.x + this.w/2, this.y + this.h/2)
+  }
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   console.log(width + " Width " + height + " Height ");
+  testButton = new button(width/2, height/2, 100, 100, "WOW")
 }
 
 
 function draw() {
   background(backgroundColour);
-  
+  testButton.show()
 }
 
