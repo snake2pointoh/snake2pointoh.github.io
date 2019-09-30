7c8970005bfc9efd8385812f60e44db40fe7933b
snake2pointoh <snake2pointoh@users.noreply.github.com>
2019-09-30 14:31:31 -0600

    added test

---
+++ cubeCollisionTest/sketch.js
@@ -0,0 +1,39 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+let backgroundColour = 220;
+let redAmmount = 0;
+let redChangeAmmount = 1;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(backgroundColour);
+}
+
+function draw() {
+  fill(redAmmount,0,0);
+  noStroke();
+  if(mouseIsPressed){
+    if(mouseButton === LEFT){
+      //noCursor();
+      ellipse(mouseX, mouseY, 100, 100);
+      
+      redAmmount += redChangeAmmount;
+      
+      if(redAmmount >= 256 || redAmmount <= 0){
+        redChangeAmmount *= -1;
+      }
+    }
+    else if(mouseButton === CENTER){
+      fill(backgroundColour);
+      ellipse(mouseX, mouseY, 100, 100);
+    }
+  }
+  console.log(redAmmount);
+}
+
