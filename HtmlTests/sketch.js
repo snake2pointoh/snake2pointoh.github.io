a4873076337786e670796c083a01ee439f1b1ec8
snake2pointoh <45461838+snake2pointoh@users.noreply.github.com>
2019-10-15 14:04:08 -0600

    cannon demo fixed

--- HtmlTests/sketch.js
+++ HtmlTests/sketch.js
@@ -5,59 +5,12 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let cannonX = 75
-let cannonY = 0
-let cannonWidth = 50
-let cannonLength = 125
-let cannonAngle
-
-let bullets = []
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   console.log(width + " Width " + height + " Height ");
-  cannonY = height - 150
 }
 
 function draw() {
   background(220);
-  drawCannon()
-  updateBullets()
-}
-
-function drawCannon(){
-  push()
-  fill(220)
-  translate(cannonX, cannonY)
-  cannonAngle = atan2(mouseY - cannonY, mouseX - cannonX)
-  rotate(cannonAngle)
-  rect(0, -cannonWidth/2, cannonLength, cannonWidth)
-  ellipse(0, 0, cannonWidth + 5)
-  pop()
-}
-
-function updateBullets(){
-  push()
-  fill(100)
-  for(let thisBullet of bullets){
-    ellipse(thisBullet.x, thisBullet.y, thisBullet.rad)
-    thisBullet.y += 5*sin(thisBullet.angle)
-    thisBullet.x += 5*cos(thisBullet.angle)
-  }
-  pop()
-}
-
-function fire(){
-  var bullet = {
-    x: cannonX,
-    y: cannonY,
-    rad: cannonWidth - 10,
-    angle: cannonAngle,
-    speed: 5,
-  }
-  bullets.push(bullet)
-}
-
-function mousePressed(){
-  fire()
 }
